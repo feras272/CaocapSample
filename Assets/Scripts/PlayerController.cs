31feb1a609c2d683003f6a51e5d2f30c007036be
feras272 <ferasdev@gmail.com>
2020-01-26 04:12:09 +0300

    new game project

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public Rigidbody2D rb;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow)){
+            
+            transform.position = new Vector2(transform.position.x -2,rb.position.y);
+            
+            //transform.position = new Vector3(-2,transform.position.y,0);
+        }
+
+         if (Input.GetKeyDown(KeyCode.RightArrow)){
+            //rb.velocity = new Vector2 (2,0);
+            
+            transform.position = new Vector2 (transform.position.x +2,rb.position.y);
+            
+            //transform.position = new Vector3(2,transform.position.y,0);
+        }
+
+        transform.Translate (Vector2.up * speed * Time.deltaTime);
+    }
+
+    
+}
